632fba8e5ba1b439fefe86db1703f9c44c63957c
FernandoJR <osfer93@gmail.com>
2023-07-01 13:14:53 -0400

    owl to root

---
+++ js/playas.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items: 3,
+        loop: true,
+        margin: 10,
+        dots:true,
+        nav:true,
+    autoplay: true,
+    navigation:true,
+    autoplayTimeout: 3000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 3.66
+        }
+    }
+});
+});
