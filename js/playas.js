11dfc59b5d73501353733295056e71384c727dad
FernandoJR <osfer93@gmail.com>
2023-07-01 21:20:24 -0400

    fix

--- js/playas.js
+++ js/playas.js
@@ -1,19 +1,18 @@
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
-        items: 3,
         loop: true,
         margin: 10,
-        dots:true,
-        nav:true,
+        dots:false,
+        nav:false,
     autoplay: true,
-    navigation:true,
-    autoplayTimeout: 3000,
+    navigation:false,
+    autoplayTimeout: 2000,
     responsive: {
         0: {
             items: 1
         },
         600: {
-            items: 3
+            items: 1
         },
         1000: {
             items: 3.66
